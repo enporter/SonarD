bd9c68cb9d685ae2dfc5ef6ed7c73c57e1e072dc
enporter <enporter09@gmail.com>
2019-05-27 12:42:57 +0700

    Create MainSrc.java

---
+++ MainSrc.java
@@ -0,0 +1,33 @@
+import java.lang.Math; 
+import java.util.*;
+import java.text.*;
+public class MainSrc {
+
+	public static void main(String[] args) {
+		depth, degrees;
+
+		System.out.println("SonarD");
+		System.out.println();
+
+		Scanner askUser= new Scanner(System.in);
+
+
+		System.out.println();
+		System.out.println("Depth");
+		System.out.println("Angle degrees: ";
+		System.out.println("Diameter "
+		askUser.close();
+		System.exit(0);
+	}
+
+
+
+	public static void input(depth, degrees) {
+
+		result = Math.multiply(2*depth)*Math.tan(degrees/2));
+
+		return result;
+	}
+
+
+}
